dfec2db4f523e9f6a8f6c7144c5e8ed3db89d0cd
rowancrookstaylor <rowancrookstaylor@gmail.com>
2026-02-24 14:09:50 -0500

    `

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -41,7 +41,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="searchMusic"
+        name="Find"
         options={{
           title: 'Search',
           tabBarLabel: 'Search',
--- app/(tabs)/library.tsx
+++ app/(tabs)/library.tsx
@@ -1,11 +1,39 @@
-// app/tabs/library.tsx
+// app/tabs/index.tsx
+//import { IconSymbol } from '@/components/ui/icon-symbol';
+//import * as AuthSession from 'expo-auth-session';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
-export default function Library() {
+export default function Dashboard() {
+  //const redirect = AuthSession.makeRedirectUri();
+  //console.log(redirect);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is your library</Text>
+      <View style={styles.settingsBar}>
+        <Text style={styles.text}>
+          top bar text
+        </Text>
+      </View>
+
+      {/*Element list*/}
+      <ScrollView contentContainerStyle={styles.container}>
+
+        {/*New Music*/}
+      <View style={styles.element}>
+        <Text style={styles.text}>
+          New Music 
+        </Text>
+      </View>
+      
+        {/*Element list*/}
+      <View style={styles.element}>
+        <Text style={styles.text}>
+          otherefefsef
+        </Text>
+      </View>
+
+    </ScrollView>
     </View>
   );
 }
@@ -16,10 +44,36 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',       // black background
     justifyContent: 'center',       // center vertically
     alignItems: 'center',           // center horizontally
+    width: '100%',
+  },
+  element:{
+    backgroundColor: '#292929',
+    paddingVertical: 20,
+    paddingHorizontal: 140,
+    borderRadius: 12,
+    marginBottom: 16,
+
   },
   text: {
-    color: 'white',                 // white text for contrast
-    fontSize: 24,
+    color: 'white',                 
+    fontSize: 10,
+    marginLeft: 10,
     fontWeight: 'bold',
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8,
+  },
+  settingsBar: {
+     height: 90,
+     width: '100%',
+    backgroundColor: '#00000',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#333',
+
+  }
+
 });
